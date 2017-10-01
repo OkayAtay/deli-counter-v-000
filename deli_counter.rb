a772cf897d80d8a41969fcf8273b194841257a01
Araba Taylor <araba.taylor20@gmail.com>
2017-10-01 23:32:37 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,29 @@
-# Write your code here.
+def line(array)
+  if array.length==0
+    puts "The line is currently empty."
+  else
+    message = "The line is currently: "
+    new_array<<array.each_with_index{|name, i| message+= "#{i+1}. #{name}" }
+    puts new_array
+  end
+end
+
+
+def take_a_number(array, name)
+  if array.length==0
+    array<<name
+    puts "Welcome, #{name}. You are number 1 in line."
+  else
+    i=array.length
+    array<<name
+    puts "Welcome, #{name}. You are number #{i+1} in line."
+  end
+end
+
+def now_serving(array)
+  if array.length==0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{array.shift}."
+  end
+end
