2c68866e6ba3fc643f00b688479dac3b924e8aa2
Araba Taylor <araba.taylor20@gmail.com>
2017-10-01 23:33:44 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,6 +4,7 @@ def line(array)
   else
     message = "The line is currently: "
     new_array = array.each_with_index.collect{|name, i| message+= "#{i+1}. #{name}" }
+    puts new_array
   end
 end
 
